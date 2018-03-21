b42aa78e0e6879170d6e5a5b9c6cf4334eaf3225
Maximilian Stoiber <contact@mxstbr.com>
2018-03-21 16:56:47 +0100

    Move dangerfile.issues.js here

---
+++ dangerfile.issues.js
@@ -0,0 +1,27 @@
+import { schedule, danger, fail } from 'danger';
+import labels from 'danger-plugin-labels';
+
+schedule(
+  labels({
+    labels: {
+      'bug report': 'Bug',
+      'feature idea': 'Feature',
+      'technical discussion': 'TBD: Tech Debt',
+    },
+  })
+);
+
+const CHECKBOXES = /^[\t ]*-[\t ]*\[x\][\t ]*(.+?)$/gim
+schedule(async () => {
+  const { body, number } = danger.github.issue;
+  const { repo: name, owner: { owner: login } } = danger.github.repository;
+  const checkedBoxes = body.match(CHECKBOXES);
+
+  await danger.github.api.issues.edit({
+    owner,
+    repo,
+    number,
+    state: !checkedBoxes ? 'closed' : 'open'
+  })
+  fail('This issue does not follow the issue template, please edit it before reopening it.')
+})
