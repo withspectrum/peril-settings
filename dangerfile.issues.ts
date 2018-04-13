7665a821d8598629c300fafe93e266af12c50128
Maximilian Stoiber <contact@mxstbr.com>
2018-04-13 13:37:08 +0200

    Switch to updated danger-plugin-labels setup, move flow setup

--- dangerfile.issues.ts
+++ dangerfile.issues.ts
@@ -3,29 +3,30 @@ import labels from 'danger-plugin-labels';
 
 schedule(
   labels({
-    labels: {
-      'bug report': 'Bug',
-      'feature idea': 'Feature',
-      'technical discussion': 'TBD: Tech Debt',
-    },
+    rules: [{
+      match: /bug\sreport/i,
+      label: 'Bug'
+    }, {
+      match: /feature(\sidea)?/i,
+      label: 'Feature'
+    }, {
+      match: /technical\sdiscussion/i,
+      label: 'TBD: Tech Debt'
+    }, {
+      match: /^question/i,
+      label: 'Question'
+    }],
+    validate: async (labels) => {
+      if (labels.includes('Question')) {
+        fail('Please direct questions to https://spectrum.chat/spectrum/open, thank you!');
+        return false;
+      }
+      if (labels.length !== 1) {
+        fail('Please check one label in the issue template.');
+        return false;
+      }
+
+      return true;
+    }
   })
 );
-
-const CHECKBOXES = /^[\t ]*-[\t ]*\[x\][\t ]*(.+?)$/gim
-schedule(async () => {
-  const { body, number, state } = danger.github.issue;
-  const { name: repo, owner: { login: owner } } = danger.github.repository;
-  const checkedBoxes = body.match(CHECKBOXES);
-
-  const newState = checkedBoxes ? 'open' : 'closed';
-
-  if (state === 'open' && newState !== state) {
-    await danger.github.api.issues.edit({
-      owner,
-      repo,
-      number,
-      state: newState
-    })
-    if (newState === 'closed') fail('This issue does not follow the issue template, so it will be closed automatically. Please open a new one!')
-  }
-})
--- dangerfile.prs.ts
+++ dangerfile.prs.ts
@@ -1,13 +1,32 @@
-import { schedule } from 'danger';
+import { schedule, danger } from 'danger';
 import labels from 'danger-plugin-labels';
+import flow from 'danger-plugin-flow';
 
 // Add automatic labels to the PR
 schedule(
   labels({
-    labels: {
-      wip: 'WIP: Building',
-      'needs testing': 'WIP: Needs Testing',
-      'ready for review': 'Ready for Review',
-    },
+    rules: [{
+      match: /WIP/i,
+      label: 'WIP: Building'
+    }, {
+      match: /needs testing/i,
+      label: 'WIP: Needs Testing'
+    }, 'Ready for Review']
+  })
+);
+
+// Make sure all new files are flowtyped
+schedule(
+  flow({
+    // Fail on newly created untyped files
+    created: 'fail',
+    // Warn on modified untyped files
+    modified: 'warn',
+    blacklist: [
+      'flow-typed/**/*.js',
+      'public/**/*.js',
+      'api/migrations/**/*.js',
+      'cypress/**/*.js',
+    ],
   })
 );
