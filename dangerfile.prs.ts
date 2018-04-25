5d3e5d6a8addf72dabf8d2ae45e00e72d96d95e3
Maximilian Stoiber <contact@mxstbr.com>
2018-04-25 20:57:35 +0200

    Revert "That didnt work"
    This reverts commit f8db1b39704a7c2940273b329e4e27bcaeb5b8da.

--- dangerfile.prs.ts
+++ dangerfile.prs.ts
@@ -1,6 +1,6 @@
 import { schedule, danger } from 'danger';
 import labels from 'danger-plugin-labels';
-// import flow from 'danger-plugin-flow';
+import flow from 'danger-plugin-flow';
 
 // Add automatic labels to the PR
 schedule(
@@ -16,19 +16,19 @@ schedule(
   })
 );
 
-// // Make sure all new files are flowtyped
-// schedule(
-//   flow({
-//     // Fail on newly created untyped files
-//     created: 'fail',
-//     // Warn on modified untyped files
-//     modified: 'warn',
-//     blacklist: [
-//       'flow-typed/**/*.js',
-//       'public/**/*.js',
-//       'api/migrations/**/*.js',
-//       'cypress/**/*.js'
-//     ]
-//   })
-// );
+// Make sure all new files are flowtyped
+schedule(
+  flow({
+    // Fail on newly created untyped files
+    created: 'fail',
+    // Warn on modified untyped files
+    modified: 'warn',
+    blacklist: [
+      'flow-typed/**/*.js',
+      'public/**/*.js',
+      'api/migrations/**/*.js',
+      'cypress/**/*.js'
+    ]
+  })
+);
 
