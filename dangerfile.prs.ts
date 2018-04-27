02987da992e741d8e748749ec11968d1c6d60d18
Maximilian Stoiber <contact@mxstbr.com>
2018-04-27 16:32:54 +0200

    Add dangerpluginnoconsole

--- dangerfile.prs.ts
+++ dangerfile.prs.ts
@@ -1,6 +1,7 @@
 import { schedule, danger } from 'danger';
 import labels from 'danger-plugin-labels';
 import flow from 'danger-plugin-flow';
+import noConsole from 'danger-plugin-no-console';
 
 // Add automatic labels to the PR
 schedule(
@@ -16,7 +17,7 @@ schedule(
   })
 );
 
-// // Make sure all new files are flowtyped
+// Make sure all new files are flowtyped
 schedule(
   flow({
     // Fail on newly created untyped files
@@ -32,3 +33,5 @@ schedule(
   })
 );
 
+// Make sure no console.logs are left in the code
+schedule(noConsole({ whitelist: ['error', 'warn'] }));
